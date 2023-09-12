e6392aff0a85d8314569e9883c1027fe29ba1a20
Suanna01 <yangnony01@gmail.com>
2023-09-12 14:04:56 +0900

    Feat : 환경 세팅

---
+++ src/main/java/com/limvengers/savehicle/SavehicleApplication.java
@@ -0,0 +1,13 @@
+package com.limvengers.savehicle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SavehicleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SavehicleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/limvengers/savehicle/SavehicleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.limvengers.savehicle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SavehicleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
